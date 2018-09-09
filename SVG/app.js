40f5ab794a2b4ebc37d87bae96050bf3ddc88ec4
Kalliarolunni <gustigaur@gmail.com>
2018-09-09 20:52:06 +0000

    buinn

--- SVG/app.js
+++ SVG/app.js
@@ -1,13 +1,19 @@
+//notaði Snap.svg til að gera stafina
+//tek fram stærð stafsins (x,y)
 a=Snap(150,187);
 g=Snap(150,187);
 u=Snap(150,187);
 s=Snap(150,187);
 t=Snap(150,187);
 
+//bý til functionið run
 function run(){
+	//bý til byrjunar stað fyrir stafinn A
+	//þetta gefur errors en skaðar ekkert framistöðuna
 	var patha=a.path("M100 -12,50")
+	//svo animate-a ég þetta þannig þetta verður að stafnum A eftir 0,5 sek
 	patha.animate({d:"M5,90 l30,-80 30,80 M20,50 l30,0"},500)
-
+	//og endurtek
 	var pathg=g.path("M37 -12,50")
 	pathg.animate({d:"M60,10 C-10,0 -10,100 60,90 l0,-40 -20,0"},1000)
 
@@ -21,4 +27,9 @@ function run(){
 	patht.animate({d:"M5,5 l50,0 -25,0 0,85"},2500)
 }
 
-setInterval(run, 2600);
+
+//kalla í functionið run
+run();
+
+//þar sem ég fann ekki leið til að láta notandan gera einhvað með stafina bætti ég við línu sem stækkar og
+//snýst 90gráður niður og verður tíu sinnum stærri þegar hoverað er yfir hana, þetta gerði ég með css
