0154406c327483750b05eb8fec7592fd1df7779f
Kalliarolunni <gustigaur@gmail.com>
2018-10-05 10:56:43 +0000

    2 adrir hringir og hreyfast ef ytt er a space

--- WebGL_3D/Scripts/app.js
+++ WebGL_3D/Scripts/app.js
@@ -2,7 +2,7 @@ var scene = new THREE.Scene();
 
 var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-var light = new THREE.PointLight(0xFFDE00,1.0);
+var light = new THREE.PointLight(0xFFFFFF,1.0);
 light.position.set(100, 100, 50);
 scene.add(light);
 
@@ -31,10 +31,23 @@ var material = new THREE.MeshBasicMaterial({
   color: "rgb(64, 224, 208)",
   wireframe: true
 });
+
 var geometry1 = new THREE.SphereGeometry(radius/2, segments/2, rings/2);
 var material1 = new THREE.MeshBasicMaterial({
   color: "rgb(255, 0, 0)",
   wireframe: true
+});
+
+var geometry2 = new THREE.SphereGeometry(radius/4, segments/4, rings/4);
+var material2 = new THREE.MeshBasicMaterial({
+  color: "rgb(255, 255, 0)",
+  wireframe: true
+});
+
+var geometry3 = new THREE.SphereGeometry(radius/8, segments/8, rings/8);
+var material3 = new THREE.MeshBasicMaterial({
+  color: "rgb(0, 255, 0)",
+  wireframe: false
 });
 
 var cube = new THREE.Mesh(geometry, material);
@@ -43,23 +56,36 @@ scene.add(cube);
 var cube1 = new THREE.Mesh(geometry1, material1);
 scene.add(cube1);
 
-camera.translateZ( distance );
+var cube2 = new THREE.Mesh(geometry2, material2);
+scene.add(cube2);
 
+var cube3 = new THREE.Mesh(geometry3, material3);
+scene.add(cube3);
+
+camera.translateZ( distance );
 
 var render = function() {
   	requestAnimationFrame(render);
-  	t += 0.01;
+  	t = t;
   	if (32 in keysDown) {
-        t-=0.01;
+        t+=0.01;
       }
-  	cube.rotation.x += 0.03;
+  	cube.rotation.x += 0.04;
   	cube.rotation.y += 0.01;
   	cube.position.x = 7*Math.sin(t*2);
   	cube.position.y = -7*Math.sin(t*2);
-  	cube1.rotation.x += 0.02;
+  	cube1.rotation.x += 0.03;
   	cube1.rotation.y += 0.06;
-  	cube1.position.x = 7*Math.sin(t*2);
+  	cube1.position.x  = 7*Math.sin(t*2);
   	cube1.position.y = -7*Math.sin(t*2);
+  	cube2.rotation.x += 0.02;
+  	cube2.rotation.y += 0.03;
+  	cube2.position.x = 7*Math.sin(t*2);
+  	cube2.position.y = -7*Math.sin(t*2);
+  	cube3.rotation.x += 0.01;
+  	cube3.rotation.y += 0.01;
+  	cube3.position.x = 7*Math.sin(t*2);
+  	cube3.position.y = -7*Math.sin(t*2);
 
   	renderer.render(scene, camera);
 };
